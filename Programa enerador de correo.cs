c18d1dc0dbbaf3a71911b4fab997e3d9debbab88
Allanrecinos <136125569+Allanrecinos@users.noreply.github.com>
2024-03-09 12:49:36 -0600

    Add files via upload

---
+++ Programa enerador de correo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Mostrar mensaje de bienvenida
+            Console.WriteLine("Bienvenido al programa de generación de correos electrónicos.");
+
+            // Solicitar ingreso de correo electrónico de dominio gmail
+            Console.Write("Por favor ingrese su correo electrónico de dominio gmail: ");
+            string correoGmail = Console.ReadLine();
+
+            // Generar nuevo correo con dominio miumg.edu.gt
+            string[] partesCorreo = correoGmail.Split('@');
+            if (partesCorreo.Length == 2 && partesCorreo[1] == "gmail.com")
+            {
+                string nuevoCorreo = partesCorreo[0] + "@miumg.edu.gt";
+                Console.WriteLine($"El nuevo correo generado es: {nuevoCorreo}");
+            }
+            else
+            {
+                Console.WriteLine("El correo electrónico ingresado no es válido o no pertenece a gmail.");
+            }
+        }
+    }
+}
---
+++ programa precios.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bienvenido a la calculadora de precios");
+
+        double[] precios = new double[3];
+
+        for (int i = 0; i < 3; i++)
+        {
+            Console.Write($"Ingrese el precio del producto {i + 1}: ");
+            precios[i] = Convert.ToDouble(Console.ReadLine());
+
+            if (precios[i] > 100.00)
+            {
+                double descuento = CalcularDescuento(precios[i]);
+                precios[i] -= descuento;
+            }
+        }
+
+        double totalBruto = 0;
+        foreach (double precio in precios)
+        {
+            totalBruto += precio;
+        }
+
+        Console.WriteLine($"El total de la compra bruto es: {totalBruto:C}");
+
+        double descuentoTotal = CalcularDescuentoTotal(precios);
+        Console.WriteLine($"El monto que te ahorras por los descuentos es: {descuentoTotal:C}");
+    }
+
+    static double CalcularDescuento(double precio)
+    {
+        return precio * 0.15;
+    }
+
+    static double CalcularDescuentoTotal(double[] precios)
+    {
+        double totalSinDescuento = 0;
+        foreach (double precio in precios)
+        {
+            totalSinDescuento += precio;
+        }
+
+        double totalConDescuento = 0;
+        foreach (double precio in precios)
+        {
+            if (precio > 100.00)
+            {
+                totalConDescuento += precio - (precio * 0.15);
+            }
+            else
+            {
+                totalConDescuento += precio;
+            }
+        }
+
+        return totalSinDescuento - totalConDescuento;
+    }
+}
